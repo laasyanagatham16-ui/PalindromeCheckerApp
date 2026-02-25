58f588b843c55495eb3354d54b0d4605fd8ba986
LAASYA REDDY N <laasyanagatham16@gmail.com>
2026-02-25 11:06:08 +0530

    Initial commit

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,9 +1,9 @@
-public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        public static void main(String[] args) {
-            System.out.println("Welcome to the palindrome checker management system");
-            System.out.println("Version : 1.0");
-            System.out.println("SYstem initialized Successfully");
+public static void main(String[] args) {
+                    System.out.println("Welcome to the Palindrome Checker Application");
+                    System.out.println("Version : 1.0");
+                    System.out.println("System initialized successfully");
+                }
+            }
         }
     }
 }
