ae07c8a1022c3b231b5c0bfe25bc386e70d7da5b
LAASYA REDDY N <laasyanagatham16@gmail.com>
2026-03-05 07:42:10 +0530

    Added UC10 Case Insensitive Space Ignored Palindrome

---
+++ src/UseCase10PalindromeCheckerApp.java
@@ -0,0 +1,31 @@
+public class UseCase10PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Original input
+        String input = "A man a plan a canal Panama";
+
+        // Normalize string: remove spaces and convert to lowercase
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+
+        boolean isPalindrome = true;
+
+        int start = 0;
+        int end = normalized.length() - 1;
+
+        // Compare characters
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        // Print result
+        System.out.println("Input : " + input);
+        System.out.println("Normalized : " + normalized);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
