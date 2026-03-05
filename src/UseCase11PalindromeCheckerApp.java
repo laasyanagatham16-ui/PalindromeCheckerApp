fed8f66d8427b3200f38701b27ba42bf1e4adbc0
LAASYA REDDY N <laasyanagatham16@gmail.com>
2026-03-05 07:43:58 +0530

    Added UC11 Object Oriented Palindrome Service

---
+++ src/UseCase11PalindromeCheckerApp.java
@@ -0,0 +1,35 @@
+class PalindromeChecker {
+
+    // Method to check palindrome
+    public boolean checkPalindrome(String word) {
+
+        int start = 0;
+        int end = word.length() - 1;
+
+        while (start < end) {
+            if (word.charAt(start) != word.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
+
+public class UseCase11PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+
+        // Create object of PalindromeChecker
+        PalindromeChecker checker = new PalindromeChecker();
+
+        boolean result = checker.checkPalindrome(input);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
+    }
+}
