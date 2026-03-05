fa45c64ed274614d0a0047644ce9fa9f492fd7fe
LAASYA REDDY N <laasyanagatham16@gmail.com>
2026-03-05 07:01:56 +0530

    Added Palindrome Checker App Use Cases

---
+++ src/UC3_UserInputPalindrome.java
@@ -0,0 +1,22 @@
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String word = "madam";
+
+        String reversed = "";
+
+        // Reverse the string
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check palindrome
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is Not a Palindrome");
+        }
+    }
+}
