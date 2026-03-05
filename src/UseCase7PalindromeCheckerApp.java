1831a5181f4f0dd93e6b2eed706f9fa24fd50e51
LAASYA REDDY N <laasyanagatham16@gmail.com>
2026-03-05 07:34:25 +0530

    Added UC7 Deque Based Palindrome Checker

---
+++ src/UseCase7PalindromeCheckerApp.java
@@ -0,0 +1,36 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class UseCase7PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Define the input string
+        String input = "civic";
+
+        // Create a Deque
+        Deque<Character> deque = new LinkedList<>();
+
+        // Insert characters into deque
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare front and rear characters
+        while (deque.size() > 1) {
+            char first = deque.removeFirst();
+            char last = deque.removeLast();
+
+            if (first != last) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
